f4e7b4d35bbf8a65dca1e16e0c28bf3426ba6d6c
Chiran <chiran@tickledmedia.com>
2017-08-22 17:25:33 +0530

    Countdown form added

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,10 +4,9 @@ var React = require('react'),
 var Main = (props) => {
   return (
     <div>
-      <div>
-        <div>
-            <Nav />
-            <p>Main.JSX rendered</p>
+      <Nav />
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
             {props.children}
         </div>
       </div>
