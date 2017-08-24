030d02cac95a2f0c4c40d1ec5f962ae307e24ebf
Chiran <chiran@tickledmedia.com>
2017-08-24 15:25:19 +0530

    Support foundation setting

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,8 @@ var Main = (props) => {
   return (
     <div>
       <Nav />
-      <div className="row">
-        <div className="columns medium-6 large-4 small-centered">
+      <div className="grid-x">
+        <div className="cell medium-6 large-4 small-centered">
             {props.children}
         </div>
       </div>
